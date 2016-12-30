617ae318ed932e8849d51d157b3aee3947845d28
pylipp <beth.aleph@yahoo.de>
2016-12-30 19:54:14 +0100

    More properties, __eq__, default init args for Chip.

--- pyrummy/chips.py
+++ pyrummy/chips.py
@@ -6,11 +6,14 @@ class Chip(object):
     RED = 1
     BLUE = 2
     BLACK = 3
+    NR_COLORS = 4
+
+    MAX_VALUE = 13
 
     POOL = 6
     YARDS = 7
 
-    def __init__(self, color, value, location, index):
+    def __init__(self, color, value, location=POOL, index=0):
         self._color = color
         self._value = value
         self._location = location
@@ -26,6 +29,14 @@ class Chip(object):
         return cls(color, value, location, index)
 
     @property
+    def value(self):
+        return self._value
+
+    @property
+    def color(self):
+        return self._color
+
+    @property
     def location(self):
         return self._location
 
@@ -36,3 +47,6 @@ class Chip(object):
     def __hash__(self):
         return self._value + (self._color << 5) + (self._location << 7) +\
                 (self._index << 10)
+
+    def __eq__(self, other):
+        return self._color == other._color and self._value == other._value
