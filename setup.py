aec3440d0415ff75799ee31ccbfe2ff087360657
pylipp <beth.aleph@yahoo.de>
2016-12-30 18:25:25 +0100

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="pyrummy",
+        version="0.1",
+        description="Rummy Computer written in Python",
+        url="http://github.com/pylipp/pyrummy",
+        author="Philipp Metzner",
+        author_email="beth.aleph@yahoo.de",
+        license="GPLv3",
+        #classifiers=[],
+        packages=find_packages(exclude=["test", "doc"]),
+        entry_points = {
+            "console_scripts": ["pyrummy = pyrummy.main:main"]
+            },
+        install_requires=[]
+        )
