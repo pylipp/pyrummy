ac1a5c81671e3c021ae386fdd5e4901d19f7513f
pylipp <beth.aleph@yahoo.de>
2016-12-31 01:40:54 +0100

    Add Player.play() method.

--- pyrummy/game.py
+++ pyrummy/game.py
@@ -1,5 +1,23 @@
-from pyrummy.chips import Chip
+from operator import attrgetter
+from itertools import chain
 
+from pyrummy.chips import Chip, Run, Book
+
+
+class _Constellation(object):
+
+    def __init__(self):
+        self.combinations = []
+        self.rest = None
+
+    @property
+    def value(self):
+        return sum([c.value for c in self.combinations])
+
+    def combination_chips(self):
+        for combi in self.combinations:
+            for chip in combi:
+                yield chip
 
 class Player(object):
 
@@ -15,6 +33,7 @@ class Player(object):
         self._game = game
         self._status = Player.HAND_ONLY
         self._drop_chip = None
+        self._yard = []
 
     def victorious(self):
         return len(self._hand) == 0
@@ -29,3 +48,60 @@ class Player(object):
         self._hand.remove(chip)
         self._drop_chip = None
         return chip
+
+    def play(self):
+        constellations = set()
+        pool = sorted(self._hand, key=attrgetter("value"))[::-1]
+
+        for p in range(len(pool)):
+            # copy the pool, subpool will be modified
+            subpool = [c for c in pool[p:]]
+            constellation = _Constellation()
+
+            while len(subpool):
+                highest_chip = subpool[0]
+                highest_chip.status = Chip.EVALUATED
+
+                candidates = list(highest_chip.candidates())
+                for chip in subpool[1:]:
+                    if chip in candidates:
+                        i = subpool.index(chip)
+                        subpool.remove(chip)
+                        if chip.color == highest_chip.color:
+                            pair = Run(highest_chip, chip)
+                        else:
+                            pair = Book(highest_chip, chip)
+                        pair_candidates = list(pair.candidates())
+                        combination = None
+                        for cchip in subpool:
+                            if cchip in pair_candidates:
+                                highest_chip.status = Chip.COMBINED
+                                chip.status = Chip.COMBINED
+                                cchip.status = Chip.COMBINED
+                                combination = pair.__class__(
+                                        highest_chip, chip, cchip)
+                                break
+                        if combination is not None:
+                            constellation.combinations.append(combination)
+                            break
+                        else:
+                            subpool.insert(i, chip)
+                subpool = [c for c in subpool if c.status == Chip.UNUSED]
+            # TODO: search rest for combinations longer than 3 chips
+            # FIXME: this needs the chip index for correctness
+            constellation.rest = [c for c in pool if c not in
+                    constellation.combination_chips()]
+            constellations.add(constellation)
+
+        # TODO: best constellation is actually the one that contains most chips
+        constellations = list(constellations)
+        constellations_values = [c.value for c in constellations]
+        max_index = constellations_values.index(max(constellations_values))
+
+        best_constellation = constellations[max_index]
+        self._yard.extend(best_constellation.combinations)
+
+        for c in best_constellation.combination_chips():
+            self._hand.remove(c)
+
+        # TODO: determine chip to drop from constellation rest
--- test/test_game.py
+++ test/test_game.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pyrummy.chips import Chip
+from pyrummy.chips import Chip, Book
 from pyrummy.game import Player
 
 
@@ -21,6 +21,43 @@ class PlayerInitTestCase(unittest.TestCase):
     def test_hand(self):
         self.player.draw(Chip.from_str("k10"))
         self.assertTrue(all([chip.location == 0 for chip in self.player._hand]))
+
+class PlayerSimplePlayTestCase(unittest.TestCase):
+    def test_yard(self):
+        player = Player(0, [
+            Chip.from_str("b8"), Chip.from_str("b7"),
+            Chip.from_str("r7"), Chip.from_str("y7")])
+        player.play()
+        book = player._yard[0]
+        self.assertEqual(1, len(player._yard))
+        self.assertEqual(3, len(book))
+        self.assertTrue(all([c.value == 7 for c in book]))
+        self.assertSetEqual({Chip.YELLOW, Chip.BLUE, Chip.RED},
+                {c.color for c in book})
+        self.assertEqual(Chip.from_str("b8"), player._hand[-1])
+        # self.assertListEqual(player._yard, [Book(Chip.from_str("b7"),
+        #                 Chip.from_str("r7"), Chip.from_str("y7"))])
+
+    def test_yard_again(self):
+        player = Player(0, [
+            Chip.from_str("r9"), Chip.from_str("r8"), Chip.from_str("r7"),
+            Chip.from_str("b8"), Chip.from_str("y8"),
+            Chip.from_str("y7"), Chip.from_str("y6"),
+            Chip.from_str("k11"), Chip.from_str("b2"), Chip.from_str("b1")])
+        player.play()
+        combi = player._yard[0]
+        # algorithm picks either r987 or rby8
+        self.assertEqual(24, combi.value)
+        self.assertEqual(7, len(player._hand))
+
+    def test_yard_double_chip(self):
+        player = Player(0, [
+            Chip.from_str("y2", index=0), Chip.from_str("y3"), Chip.from_str("y4"),
+            Chip.from_str("y2", index=1), Chip.from_str("r2"), Chip.from_str("k2")])
+        # import pdb; pdb.set_trace()
+        player.play()
+        self.assertEqual(2, len(player._yard))
+        self.assertEqual(0, len(player._hand))
 
 if __name__ == "__main__":
     unittest.main()
